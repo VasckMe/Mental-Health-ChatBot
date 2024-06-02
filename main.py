050908a4664e05520e152cbfdbc57339ec932892
A Kasarin <>
2024-06-02 15:06:54 +0200

    [MH-003] Dependencies
    Add imports with frameworks
    Add import with our own separated files

---
+++ main.py
@@ -0,0 +1,5 @@
+# required modules 
+import constants
+import services.json_service as json_service
+import services.pickle_service as pickle_service
+import services.ml_model_service as model_service
---
+++ model.py
@@ -0,0 +1,11 @@
+# required modules 
+import random 
+import numpy as np 
+import constants
+import variables
+import services.json_service as json_service
+import services.addiction_service as addiction_service
+import services.pickle_service as pickle_service
+import services.ml_model_service as model_service
+from nltk.stem import WordNetLemmatizer
+
