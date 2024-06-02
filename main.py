92d98cc39706bb58c28ed5cc2b5facef9185b418
A Kasarin <>
2024-06-02 15:22:50 +0200

    Add tasks marks

--- main.py
+++ main.py
@@ -3,3 +3,15 @@ import constants
 import services.json_service as json_service
 import services.pickle_service as pickle_service
 import services.ml_model_service as model_service
+
+# [011] Load saved intents, words, classes and ML Model
+
+# [012] Write function to clean up sentences and function for checking, if word is present in input and return array with '0' and '1'
+
+# [013] Write predict function
+
+# [014] Write function for getting response
+
+# [015] Write function to make infinite chatting
+
+# [016] Add logic to task [015] that can interrupt program in 'bye', 'goodbye' etc. cases
--- model.py
+++ model.py
@@ -9,3 +9,15 @@ import services.pickle_service as pickle_service
 import services.ml_model_service as model_service
 from nltk.stem import WordNetLemmatizer
 
+# [004] Implement function to read json dataset with given dataset name
+
+# [005] Add logic with to divide dataset text into words, classes and documents
+
+# [006] Write logic for saving words and classes into binary files in folder ‘saved_data’ 
+
+# [007] Make binary code from input dataset data
+
+# [008] Write code for creating instance (model) with Sequential and configure it
+
+# [009] Compile and save ML Model. Add print in the end of the code to show the end of the 
+# compiling
