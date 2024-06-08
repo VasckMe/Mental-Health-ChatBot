bc2aae32710eca6f44986a600820b7340eb407e3
anton <kasarin.anton@gmail.com>
2024-06-08 14:52:10 +0200

    Add function for [MH-008]

--- services/ml_model_service.py
+++ services/ml_model_service.py
@@ -5,4 +5,14 @@ import services.addiction_service as addiction_service
 from keras.optimizers import SGD 
 from keras.layers import Dense, Dropout 
 from keras.models import Sequential 
-from keras.models import load_model 
+from keras.models import load_model
+
+# create ML model instance (object)
+def create(data_x, data_y):
+    model = Sequential() 
+    model.add(Dense(128, input_shape=(len(data_x[0]), ), activation='relu')) 
+    model.add(Dropout(0.5)) 
+    model.add(Dense(64, activation='relu')) 
+    model.add(Dropout(0.5)) 
+    model.add(Dense(len(data_y[0]), activation='softmax'))  
+    return model
