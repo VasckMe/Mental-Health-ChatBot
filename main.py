717e74aea14bc4e4f6a9c78c4a0da5fc9b26a409
AoDem0 <blue.demon.4569@gmail.com>
2024-06-08 17:35:42 +0200

    [MH-015]-Infinite-chatting
    adding infinite chatting to code

--- main.py
+++ main.py
@@ -8,10 +8,12 @@ import services.ml_model_service as model_service
 
 # [012] Write function to clean up sentences and function for checking, if word is present in input and return array with '0' and '1'
 
-# [013] Write predict function
-
-# [014] Write function for getting response
-
 # [015] Write function to make infinite chatting
+while True:
+    userText = input('User: ')
+
+    # [013] Write predict function
+
+    # [014] Write function for getting response
 
 # [016] Add logic to task [015] that can interrupt program in 'bye', 'goodbye' etc. cases
